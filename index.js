2b3c1f3a3e6e2ca2d2e47cb4e40a0d936af07bf7
Robert Hameetman <rob@rham.io>
2017-10-18 13:10:52 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * index.js
+ * Robert Hameetman
+ * 10/18/17
+ *
+ * My component module
+ *
+ * @module my/module
+ * @see module:my/othermodule
+ */
+'use strict';
+require('babel-core/register');
+require('./server');
